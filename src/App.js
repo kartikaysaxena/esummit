ce29a1c0a4e0a196f83072df60891228187a84c2
Kartikay <kartikay_2101ce32@iitp.ac.in>
2024-03-08 13:30:39 +0530

    close
    Signed-off-by: Kartikay <kartikay_2101ce32@iitp.ac.in>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import Esummit from './pages/esummit';
 import Events from './pages/Events';
 
 function App() {
+  // const reload = () => window.location.reload();
+  // const navigate = useNavigate();  
+
+
+  // function navigateToMain() {
+  //   navigate('/esummit');
+  // }  
+
+  // function navigateToEsummit() {
+  //   window.location.href = "/esummit";
+  // }
+
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Esummit />} />
+        <Route path="/" element={<Esummit/>} />
+        <Route path='/esummit' element={<Navigate to="/"/>}></Route>
        </Routes>
     </Router>
   );
